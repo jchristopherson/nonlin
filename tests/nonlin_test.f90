17b51911ebebf899805f7e73977b924912f9b0d2
jchristopherson <jchristopherson@hotmail.com>
2017-06-15 16:01:00 -0500

    Add testing

---
+++ tests/nonlin_test.f90
@@ -0,0 +1,9 @@
+! nonlin_test.f90
+
+! The testing application for the NONLIN library.
+program main
+    ! Imported Modules
+
+    ! Introduce the testing application
+    print '(A)', "Hello from the NONLIN test application."
+end program
