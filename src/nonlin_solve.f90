79f2ffa10e235ea1b2329b331bfa1d8df53b85db
jchristopherson <jchristopherson@hotmail.com>
2017-06-15 14:17:41 -0500

    Finished defining the quasi-newton solver

--- src/nonlin_solve.f90
+++ src/nonlin_solve.f90
@@ -10,9 +10,13 @@ module nonlin_solve
     use nonlin_types
     use nonlin_linesearch, only : line_search
     use ferror, only : errors
+    use linalg_factor, only : qr_factor, form_qr, qr_rank1_update
+    use linalg_core, only : rank1_update, mtx_mult
+    use linalg_solve, only : solve_triangular_system
     implicit none
     private
     public :: equation_solver
+    public :: line_search_solver
     public :: nonlin_solver
 
 ! ******************************************************************************
@@ -260,7 +264,39 @@ contains
 ! ******************************************************************************
 ! QUASI_NEWTON_SOLVER MEMBERS
 ! ------------------------------------------------------------------------------
-    !
+    !> @brief Applies the quasi-Newton's method developed by Broyden in
+    !! conjunction with a backtracking type line search to solve N equations
+    !! of N unknowns.
+    !!
+    !! @param[in,out] this The equation_solver-based object.
+    !! @param[in] fcn The vecfcn_helper object containing the equations to
+    !!  solve.
+    !! @param[in,out] x On input, an N-element array containing an initial
+    !!  estimate to the solution.  On output, the updated solution estimate.
+    !!  N is the number of variables.
+    !! @param[out] fvec An M-element array that, on output, will contain
+    !!  the values of each equation as evaluated at the variable values
+    !!  given in @p x.
+    !! @param[out] ib An optional output, that if provided, allows the 
+    !!  caller to obtain iteration performance statistics.
+    !! @param[out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - NL_INVALID_OPERATION_ERROR: Occurs if no equations have been defined.
+    !!  - NL_ARRAY_SIZE_ERROR: Occurs if any of the input arrays are not sized
+    !!      correctly.
+    !!  - NL_DIVERGENT_BEHAVIOR_ERROR: Occurs if the direction vector is
+    !!      pointing in an apparent uphill direction.
+    !!  - NL_CONVERGENCE_ERROR: Occurs if the line search cannot converge within
+    !!      the allowed number of iterations.
+    !!  - NL_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory 
+    !!      available.
+    !!
+    !! @par See Also
+    !! - [Wikipedia](https://en.wikipedia.org/wiki/Broyden%27s_method)
+    !! - [Numerical Recipes](http://numerical.recipes/)
     subroutine qns_solve(this, fcn, x, fvec, ib, err)
         ! Arguments
         class(quasi_newton_solver), intent(inout) :: this
@@ -275,7 +311,254 @@ contains
         real(dp), parameter :: half = 0.5d0
         real(dp), parameter :: one = 1.0d0
         real(dp), parameter :: factor = 1.0d2
+
+        ! Local Variables
+        logical :: restart, xcnvrg, fcnvrg, gcnvrg
+        integer(i32) :: i, neqn, nvar, flag, lw1, lw2, lw3, neval, iter, maxeval
+        real(dp), allocatable, dimension(:) :: work, dx, df, fvold, xold, s
+        real(dp), allocatable, dimension(:,:) :: q, r, b
+        real(dp) :: test, f, fold, alpha, temp, den, ftol, xtol, gtol, eps, &
+            stpmax, x2
+        type(iteration_behavior) :: lib
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+        character(len = 128) :: errmsg
+        class(line_search) :: ls
+
+        ! Initialization
+        restart = .true.
+        xcnvrg = .false.
+        fcnvrg = .false.
+        gcnvrg = .false.
+        neqn = fcn%get_equation_count()
+        nvar = fcn%get_variable_count()
+        neval = 0
+        iter = 0
+        ftol = this%get_fcn_tolerance()
+        xtol = this%get_var_tolerance()
+        gtol = this%get_gradient_tolerance()
+        maxeval = this%get_max_fcn_evals()
+        eps = epsilon(eps)
+        if (present(ib)) then
+            ib%iter_count = niter
+            ib%fcn_count = neval
+            ib%converge_on_fcn = fcnvrg
+            ib%converge_on_chng = xcnvrg
+            ib%converge_on_zero_diff = gcnvrg
+        end if
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        if (.not.this%is_line_search_defined()) &
+            call this%set_default_line_search()
+        ls = this%get_line_search()
+
+        ! Input Check
+        if (.not.fcn%is_fcn_defined()) then
+            ! ERROR: No function is defined
+            call errmgr%report_error("qns_solve", &
+                "No function has been defined.", &
+                NL_INVALID_OPERATION_ERROR)
+            return
+        end if
+        flag = 0
+        if (nvar /= neqn) then
+            flag = 2
+        else if (size(x) /= nvar) then
+            flag = 3
+        else if (size(fvec) /= neqn) then
+            flag = 4
+        end if
+        if (flag /= 0) then
+            ! One of the input arrays is not sized correctly
+            write(errmsg, '(AI0A)') "Input number ", flag, &
+                " is not sized correctly."
+            call errmgr%report_error("qns_solve", trim(errmsg), &
+                NL_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        ! Local Memory Allocation
+        allocate(q(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(r(neqn, nvar), stat = flag)
+        if (flag == 0) allocate(tau(min(neqn, nvar)), stat = flag)
+        if (flag == 0) allocate(b(neqn, nvar), stat = flag)
+        if (flag == 0) allocate(df(neqn), stat = flag)
+        if (flag == 0) allocate(fvold(neqn), stat = flag)
+        if (flag == 0) allocate(xold(nvar), stat = flag)
+        if (flag == 0) allocate(dx(nvar), stat = flag)
+        if (flag == 0) allocate(s(neqn), stat = flag)
+        if (flag /= 0) then
+            ! ERROR: Out of memory
+            call errmgr%report_error("qns_solve", &
+                "Insufficient memory available.", NL_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+        call qr_factor(r, tau, work, lw1)
+        call form_qr(r, tau, q, work, lw2)
+        call fcn%jacobian(x, b, fv = fvec, olwork = lw3)
+        allocate(work(max(lw1, lw2, lw3)), stat = flag)
+        if (flag /= 0) then
+            ! ERROR: Out of memory
+            call errmgr%report_error("qns_solve", &
+                "Insufficient memory available.", NL_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+
+        ! Test to see if the initial guess is a root
+        call fcn%fcn(x, fvec)
+        f = half * dot_product(fvec, fvec)
+        neval = neval + 1
+        test = zero
+        do i = 1, neqn
+            test = max(abs(fvec(i)), test)
+        end do
+        if (test < ftol) then
+            fcnvrg = .true.
+        end if
+
+        ! Process
+        flag = 0 ! Used to check for convergence errors
+        if (.not.fcnvrg) then
+            ! Determine the maximum line search step
+            stpmax = factor * max(norm2(x), real(nvar, dp))
+
+            ! Main Iteration Loop
+            do
+                ! Update the iteration counter
+                iter = iter + 1
+
+                ! Compute or update the Jacobian
+                if (restart) then
+                    ! Compute the Jacobian
+                    call fcn%jacobian(x, b, fvec, work)
+
+                    ! Compute the QR factorization, and form Q & R
+                    r = b ! Copy the Jacobian - we'll need it later
+                    call qr_factor(r, tau, work)
+                    call form_qr(r, tau, q, work)
+                else
+                    ! Apply the rank 1 update to Q and R
+                    df = fvec - fvold
+                    dx = x - xold
+                    x2 = dot_product(x, x)
+                    alpha = one / x2
+
+                    ! Compute S = B * DX
+                    s = matmul(b, dx)
+
+                    ! Compute S = ALPHA * (DF - S)
+                    s = alpha * (df - s)
+
+                    ! Compute the new Q and R matrices for the rank1 update:
+                    ! B' = B + ALPHA * S * DX**T
+                    call rank1_update(alpha, s, dx, b)
+                    call qr_rank1_update(q, r, s, dx, work)
+                    ! FYI, both S and DX are modified by qr_rank1_update
+                end if
+
+                ! Compute GRAD = B**T * F, store in DX
+                call mtx_mult(.true., one, b, fvec, zero, dx)
+
+                ! Store FVEC and X
+                xold = x
+                fvold = fvec
+                fold = f
+
+                ! Solve the linear system: B * DX = -F for DX noting that
+                !! B = Q * R.  As such, form -Q**T * F, and store in DF
+                call mtx_mult(.true., -one, q, fvec, zero, df)
+
+                ! Now we have R * DX = -Q**T * F, and since R is upper 
+                ! triangular, the solution is readily computed.  The solution
+                ! will be stored in the first NVAR elements of DF
+                call solve_triangular_system(.true., .false., .true., r, &
+                    df(1:nvar))
+                
+                ! Define the step length for the line search
+                temp = dot_product(df(1:nvar), df(1:nvar))
+                if (temp > stpmax) df(1:nvar) = df(1:nvar) * (stpmax / temp)
+
+                ! Apply the line search
+                call ls%search(fcn, xold, dx, df(1:nvar), x, fvec, fold, f, &
+                    lib, errmgr)
+                neval = neval + lib%fcn_count
+
+                ! Test for convergence
+                test = zero
+                do i = 1, neqn
+                    test = max(abs(fvec(i)), test)
+                end do
+                if (test < ftol) then
+                    fcnvrg = .true.
+                    exit
+                end if
+
+                ! Ensure we didn't come to rest on a stationary point
+                if (lib%converge_on_zero_diff) then
+                    if (restart) then
+                        ! We've already tried computing a new Jacobian, but
+                        ! that didn't help
+                        gcnvrg = .true.
+                        exit
+                    else
+                        test = zero
+                        den = max(f, half * real(nvar, dp))
+                        do i = 1, nvar
+                            temp = abs(dx(i)) * max(abs(xi(i)), one) / den
+                            test = max(test, temp)
+                        end do
+                        if (test < gtol) then
+                            gcnvrg = .true.
+                            exit
+                        else
+                            restart = .true.
+                        end if
+                    end if
+                else
+                    ! No need to compute a new Jacobian
+                    restart = .false.
+
+                    ! Test for convergence based upon change in solution
+                    test = zero
+                    do i = 1, nvar
+                        temp = abs(x(i) - xold(i)) / max(abs(x(i)), one)
+                        test = max(temp, test)
+                    end do
+                    if (test < xtol) then
+                        xcnvrg = .true.
+                        exit
+                    end if
+                end if
+
+                ! Ensure we haven't made too many function evaluations
+                if (neval >= maxeval) then
+                    flag = 1
+                    exit
+                end if
+            end do
+        end if
+
+        ! Report out iteration statistics
+        if (present(ib)) then
+            ib%iter_count = niter
+            ib%fcn_count = neval
+            ib%converge_on_fcn = fcnvrg
+            ib%converge_on_chng = xcnvrg
+            ib%converge_on_zero_diff = gcnvrg
+        end if
+
+        ! Check for convergence issues
+        if (flag /= 0) then
+            write(errmsg, '(AI0A)') "The algorithm failed to " // &
+                "converge.  Function evaluations performed: ", neval, "."
+            call errmgr%report_error("qns_solve", errmsg, &
+                NL_CONVERGENCE_ERROR)
+        end if
     end subroutine
+    
 ! ------------------------------------------------------------------------------
 
 ! ------------------------------------------------------------------------------
