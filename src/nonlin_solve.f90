b91ef0cb6267d4cf0a84e4501cd20b3ed9ab2a76
jchristopherson <jchristopherson@hotmail.com>
2017-06-15 07:35:23 -0500

    Added nonlin_solve.f90

---
+++ src/nonlin_solve.f90
@@ -0,0 +1,26 @@
+! nonlin_solve.f90
+
+!> @brief \b nonlin_solve
+!!
+!! @par Purpose
+!! To provide various routines capapble of solving systems of nonlinear 
+!! equations.
+module nonlin_solve
+    use linalg_constants, only : dp, i32
+    use nonlin_types
+    use nonlin_linesearch, only : line_search
+    use ferror, only : errors
+    implicit none
+    private
+
+contains
+! ******************************************************************************
+! QUASI-NEWTON METHOD (BROYDEN'S METHOD)
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+end module
