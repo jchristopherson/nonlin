a97dddf2323276616f5d7d02efca1bc48627524d
jchristopherson <jchristopherson@hotmail.com>
2017-06-16 18:30:44 -0500

    Added Jacobian testing

--- tests/nonlin_test.f90
+++ tests/nonlin_test.f90
@@ -3,7 +3,11 @@
 ! The testing application for the NONLIN library.
 program main
     ! Imported Modules
+    use test_jacobian
 
     ! Introduce the testing application
     print '(A)', "Hello from the NONLIN test application."
+
+    ! Tests
+    call test_jacobian_1()
 end program
