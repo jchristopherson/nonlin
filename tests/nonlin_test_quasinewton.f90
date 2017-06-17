dd2762138609681fb38601336fd73092fc2ad7a9
jchristopherson <jchristopherson@hotmail.com>
2017-06-16 20:53:12 -0500

    Added to quasi-newton test

--- tests/nonlin_test_quasinewton.f90
+++ tests/nonlin_test_quasinewton.f90
@@ -24,23 +24,43 @@ contains
         f(2) = x(1)**2 - 2.0d0 * x(2)**2 - 7.0d0
     end subroutine
 
+    ! Jacobian:
+    !
+    !     | 2x  2y |
+    ! J = |        |
+    !     | 2x  -4y|
+    subroutine jac1(x, j)
+        real(dp), intent(in), dimension(:) :: x
+        real(dp), intent(out), dimension(:,:) :: j
+        j = 2.0d0 * reshape([x(1), x(1), x(2), -2.0d0 * x(2)], [2, 2])
+    end subroutine
+
 ! ------------------------------------------------------------------------------
     subroutine test_quasinewton_1()
         ! Local Variables
         type(vecfcn_helper) :: obj
         procedure(vecfcn), pointer :: fcn
+        procedure(jacobianfcn), pointer :: jac
         type(quasi_newton_solver) :: solver
         type(iteration_behavior) :: ib
         real(dp) :: x(2), f(2)
 
         ! Initialization
         fcn => fcn1
+        jac => jac1
         call obj%set_fcn(fcn, 2, 2)
-        x = 1.0d0
+        call obj%set_jacobian(jac)
+        x = [1.0d0, 1.0d0]
 
         ! Process
         call solver%solve(obj, x, f, ib)
-        print '(AF6.3AF6.3)', "Solution 1:", x(1), ", ", x(2)
+        print '(AF7.3AF7.3)', "Solution 1:", x(1), ", ", x(2)
+        print '(AF7.3AF7.3)', "Residual 1:", f(1), ", ", f(2)
+        print '(AL)', "Converged on residual:", ib%converge_on_fcn
+        print '(AL)', "Converged on function change:", ib%converge_on_chng
+        print '(AL)', "Converge on zero gradient:", ib%converge_on_zero_diff
+        print '(AI0)', "Iterations: ", ib%iter_count
+        print '(AI0)', "Function Evaluations: ", ib%fcn_count
     end subroutine
 
 ! ------------------------------------------------------------------------------
