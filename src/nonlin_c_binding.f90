8ffc775db892d1b76127c7280c17ea5d1a1a459c
jchristopherson <jchristopherson@hotmail.com>
2017-07-01 06:25:32 -0500

    Updating the C interface

--- src/nonlin_c_binding.f90
+++ src/nonlin_c_binding.f90
@@ -43,6 +43,19 @@ interface
         real(dp), intent(in) :: x(nvar)
         real(dp), intent(out) :: f(neqn)
     end subroutine
+
+    !>  @brief The C-friendly interface to jacobianfcn.
+    !!
+    !! @param[in] neqn The number of equations.
+    !! @param[in] nvar The number of variables.
+    !! @param[in] x The NVAR-element array containing the indendent variables.
+    !! @param[out] jac An NEQN-byNVAR matrix where the Jacobian will be written.
+    subroutine cjacobianfcn(neqn, nvar, x, jac)
+        use linalg_constants, only : dp, i32
+        integer(i32), intent(in) :: neqn, nvar
+        real(dp), intent(in) :: x(nvar)
+        real(dp), intent(out) :: jac(neqn, nvar)
+    end subroutine
 end interface
 
 ! ******************************************************************************
@@ -107,31 +120,31 @@ end interface
         private
         !> A pointer to the target cvecfcn routine.
         procedure(cvecfcn), pointer, nopass :: m_cfcn => null()
+        !> A pointer to the Jacobian routine.
+        procedure(cjacobianfcn), pointer, nopass :: m_cjac => null()
     contains
         !> @brief Establishes a pointer to the routine containing the system of
         !!  equations to solve.
         procedure, public :: set_cfcn => cvfh_set_fcn
-        ! !> @brief Establishes a pointer to the routine for computing the 
-        ! !! Jacobian matrix of the system of equations.  If no routine is 
-        ! !! defined, the Jacobian matrix will be computed numerically (this is 
-        ! !! the default state).
-        ! procedure, public :: set_jacobian => vfh_set_jac
-
+        !> @brief Establishes a pointer to the routine for computing the 
+        !! Jacobian matrix of the system of equations.  If no routine is 
+        !! defined, the Jacobian matrix will be computed numerically (this is 
+        !! the default state).
+        procedure, public :: set_cjacobian => cvfh_set_jac
         !> @brief Tests if the pointer to the subroutine containing the system
         !! of equations to solve has been assigned.
         procedure, public :: is_fcn_defined => cvfh_is_fcn_defined
-        ! !> @brief Tests if the pointer to the subroutine containing the system
-        ! !! of equations to solve has been assigned.
-        ! procedure, public :: is_jacobian_defined => vfh_is_jac_defined
-
+        !> @brief Tests if the pointer to the subroutine containing the system
+        !! of equations to solve has been assigned.
+        procedure, public :: is_jacobian_defined => cvfh_is_jac_defined
         !> @brief Executes the routine containing the system of equations to
         !! solve.  No action is taken if the pointer to the subroutine has not 
         !! been defined.
         procedure, public :: fcn => cvfh_fcn
-        ! !> @brief Executes the routine containing the Jacobian matrix if 
-        ! !! supplied.  If not supplied, the Jacobian is computed via finite 
-        ! !! differences.
-        ! procedure, public :: jacobian => vfh_jac_fcn
+        !> @brief Executes the routine containing the Jacobian matrix if 
+        !! supplied.  If not supplied, the Jacobian is computed via finite 
+        !! differences.
+        procedure, public :: jacobian => cvfh_jac_fcn
     end type
 
 contains
@@ -230,10 +243,169 @@ contains
     end subroutine
 
 ! ------------------------------------------------------------------------------
+    !> @brief Establishes a pointer to the routine for computing the Jacobian
+    !! matrix of the system of equations.  If no routine is defined, the
+    !! Jacobian matrix will be computed numerically (this is the default state).
+    !!
+    !! @param[in,out] this The cvecfcn_helper object.
+    !! @param[in] jac The function pointer.
+    subroutine cvfh_set_jac(this, jac)
+        class(cvecfcn_helper), intent(inout) :: this
+        procedure(cjacobianfcn), intent(in), pointer :: jac
+        this%m_cjac => jac
+    end subroutine
 
 ! ------------------------------------------------------------------------------
+    !> @brief Tests if the pointer to the subroutine containing the system of 
+    !! equations to solve has been assigned.
+    !!
+    !! @param[in] this The vecfcn_helper object.
+    !! @return Returns true if the pointer has been assigned; else, false.
+    pure function cvfh_is_jac_defined(this) result(x)
+        class(cvecfcn_helper), intent(in) :: this
+        logical :: x
+        x = associated(this%m_cjac)
+    end function
 
 ! ------------------------------------------------------------------------------
+    !> @brief Executes the routine containing the Jacobian matrix if supplied.
+    !! If not supplied, the Jacobian is computed via finite differences.
+    !!
+    !! @param[in] this The vecfcn_helper object.
+    !! @param[in] x An N-element array containing the independent variabls 
+    !!  defining the point about which the derivatives will be calculated.
+    !! @param[out] jac An M-by-N matrix where, on output, the Jacobian will
+    !!  be written.
+    !! @param[in] fv An optional M-element array containing the function values
+    !!  at @p x.  If not supplied, the function values are computed at @p x.
+    !! @param[out] work An optional input, that if provided, prevents any local
+    !!  memory allocation.  If not provided, the memory required is allocated
+    !!  within.  If provided, the length of the array must be at least
+    !!  @p olwork.  Notice, a workspace array is only utilized if the user does
+    !!  not provide a routine for computing the Jacobian.
+    !! @param[out] olwork An optional output used to determine workspace size.
+    !!  If supplied, the routine determines the optimal size for @p work, and
+    !!  returns without performing any actual calculations.
+    !! @param[out] err An optional integer output that can be used to determine
+    !!  error status.  If not used, and an error is encountered, the routine
+    !!  simply returns silently.  If used, the following error codes identify
+    !!  error status:
+    !!  - 0: No error has occurred.
+    !!  - n: A positive integer denoting the index of an invalid input.
+    !!  - -1: Indicates internal memory allocation failed.
+    subroutine cvfh_jac_fcn(this, x, jac, fv, work, olwork, err)
+        ! Arguments
+        class(cvecfcn_helper), intent(in) :: this
+        real(dp), intent(inout), dimension(:) :: x
+        real(dp), intent(out), dimension(:,:) :: jac
+        real(dp), intent(in), dimension(:), optional, target :: fv
+        real(dp), intent(out), dimension(:), optional, target :: work
+        integer(i32), intent(out), optional :: olwork, err
+
+        ! Parameters
+        real(dp), parameter :: zero = 0.0d0
+
+        ! Local Variables
+        integer(i32) :: j, m, n, lwork, flag
+        real(dp) :: eps, epsmch, h, temp
+        real(dp), pointer, dimension(:) :: fptr, f1ptr
+        real(dp), allocatable, target, dimension(:) :: wrk
+
+        ! Initialization
+        if (present(err)) err = 0
+        m = this%get_equation_count()
+        n = this%get_variable_count()
+
+        ! Input Checking
+        flag = 0
+        if (size(x) /= n) then
+            flag = 2
+        else if (size(jac, 1) /= m .or. size(jac, 2) /= n) then
+            flag = 3
+        end if
+        if (flag /= 0) then
+            ! ERROR: Incorrectly sized input arrays
+            if (present(err)) err = flag
+            return
+        end if
+
+        ! Process
+        if (.not.this%is_fcn_defined()) return
+        if (associated(this%m_cjac)) then
+            ! Workspace Query
+            if (present(olwork)) then
+                olwork = 0
+                return
+            end if
+
+            ! Call the user-defined Jacobian routine
+            call this%m_cjac(m, n, x, jac)
+        else
+            ! Compute the Jacobian via finite differences
+            if (present(fv)) then
+                lwork = m
+            else
+                lwork = 2 * m
+            end if
+
+            if (present(olwork)) then
+                ! The user is just making a workspace query.  Simply return the
+                ! workspace length, and exit the routine.
+                olwork = lwork
+                return
+            end if
+
+            ! Local Memory Allocation
+            if (present(work)) then
+                if (size(work) < lwork) then
+                    ! ERROR: Workspace is too small
+                    if (present(err)) err = 5
+                    return
+                end if
+                f1ptr => work(1:m)
+                if (present(fv)) then
+                    if (size(fv) < m) then
+                        ! ERROR: Function vector too small
+                        if (present(err)) err = 4
+                        return
+                    end if
+                    fptr => fv(1:m)
+                else
+                    fptr => work(m+1:2*m)
+                    call this%fcn(x, fptr)
+                end if
+            else
+                allocate(wrk(lwork), stat = flag)
+                if (flag /= 0) then
+                    ! ERROR: Memory issues
+                    if (present(err)) err = -1
+                    return
+                end if
+                f1ptr => wrk(1:m)
+                if (present(fv)) then
+                    fptr => fv(1:m)
+                else
+                    fptr => wrk(m+1:2*m)
+                    call this%fcn(x, fptr)
+                end if
+            end if
+
+            ! Establish step size factors
+            epsmch = epsilon(epsmch)
+            eps = sqrt(epsmch)
+
+            ! Compute the derivatives via finite differences
+            do j = 1, n
+                temp = x(j)
+                h = eps * abs(temp)
+                if (h == zero) h = eps
+                x(j) = temp + h
+                call this%fcn(x, f1ptr)
+                x(j) = temp
+                jac(:,j) = (f1ptr - fptr) / h
+            end do
+        end if
+    end subroutine
 
 ! ******************************************************************************
 ! SOLVER ROUTINES
@@ -345,7 +517,7 @@ contains
 
         ! Local Variables
         procedure(cvecfcn), pointer :: fptr
-        procedure(jacobianfcn), pointer :: jptr
+        procedure(cjacobianfcn), pointer :: jptr
         type(errors), pointer :: eptr
         type(quasi_newton_solver) :: solver
         type(cvecfcn_helper) :: obj
@@ -357,7 +529,7 @@ contains
         call obj%set_cfcn(fptr, n, n)
         if (c_associated(jac)) then
             call c_f_procpointer(jac, jptr)
-            call obj%set_jacobian(jptr)
+            call obj%set_cjacobian(jptr)
         end if
         call solver%set_max_fcn_evals(tol%max_evals)
         call solver%set_fcn_tolerance(tol%fcn_tolerance)
