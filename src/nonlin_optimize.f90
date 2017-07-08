722d2d01804242133f04dc2f11695cf1409cb754
jchristopherson <jchristopherson@hotmail.com>
2017-07-08 07:39:04 -0500

    Added additional optimization reference links

--- src/nonlin_optimize.f90
+++ src/nonlin_optimize.f90
@@ -3,3 +3,5 @@
 ! REF:
 ! http://ab-initio.mit.edu/wiki/index.php/NLopt_Algorithms#Nelder-Mead_Simplex
 ! http://ab-initio.mit.edu/wiki/index.php/NLopt_Algorithms
+! https://scicomp.stackexchange.com/questions/14787/fortran-library-for-minimization-or-maximization-of-functions-optimization-prob
+! http://ab-initio.mit.edu/wiki/index.php/NLopt
