d1add6c0c398a4589ae39a9de854afedc0848e28
Jason Christopherson <jchristopherson@hotmail.com>
2020-03-22 16:15:30 -0500

    Update scripts

---
+++ src/nonlin_c_api.f90
@@ -0,0 +1,12 @@
+! nonlin_c_api.f90
+
+!> @brief Defines a C-friendly API to the NONLIN library.
+module nonlin_c_api
+    use iso_c_binding
+    use nonlin_constants
+    use nonlin_core
+    use nonlin_solve
+    use nonlin_least_squares
+    implicit none
+contains
+end module
