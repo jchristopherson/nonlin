8311816fc501af1ce714bd77974f7d312523d544
jchristopherson <jchristopherson@hotmail.com>
2024-02-28 21:40:30 +0000

    Deploying to gh-pages from @ jchristopherson/nonlin@1559032e583bc2fa7d8c3d5068892bc5516c7b91 🚀

--- search/all_f.js
+++ search/all_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['roots_0',['roots',['../structnonlin__polynomials_1_1polynomial.html#aabefa8eebaed14d790c39cd45c8b7b26',1,'nonlin_polynomials::polynomial']]]
+  ['roots_0',['roots',['../structnonlin__polynomials_1_1polynomial.html#af5a6b6e98bb18d327e54d213022c3112',1,'nonlin_polynomials::polynomial']]]
 ];
