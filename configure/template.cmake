786f0e0bbb295668a55c1925b3e7ed8741042d02
Jason Christopherson <jchristopherson@hotmail.com>
2026-07-16 21:25:06 -0500

    Fix dependency code

--- configure/template.cmake
+++ configure/template.cmake
@@ -1,10 +1,9 @@
 @PACKAGE_INIT@
 
-if (NOT BUILD_SHARED_LIBS)
-  include(CMakeFindDependencyMacro)
-  find_dependency(ferror QUIET)
-  find_dependency(linalg QUIET)
-endif()
+include(CMakeFindDependencyMacro)
+find_dependency(ferror QUIET)
+find_dependency(linalg QUIET)
+
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
